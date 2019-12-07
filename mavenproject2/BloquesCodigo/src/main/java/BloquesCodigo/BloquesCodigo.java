943db4e93021638f13a7fcb2d4710c90394f9619
caipipoca <caipipoca@caipipoca-X553MA>
2019-12-07 21:18:26 +0100

    Ejemplos Enumeraciones

---
+++ mavenproject2/BloquesCodigo/src/main/java/BloquesCodigo/BloquesCodigo.java
@@ -0,0 +1,12 @@
+
+package BloquesCodigo;
+
+
+public class BloquesCodigo {
+    public static void main(String[] args) {
+        
+        Persona p1 = new Persona();
+        int idPersona = p1.getIdPersona();
+        System.out.println("idPersona = " + idPersona);
+    }
+}
